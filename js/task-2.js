63d0966dec477e4d499b27ab21cd3e0a5f9d7da2
BelSerhij <belkasergij@gmail.com>
2025-12-11 01:19:37 +0200

    hw-3

---
+++ js/task-2.js
@@ -0,0 +1,19 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    const combi = firstArray.concat(secondArray);
+    if (combi.length > maxLength) {
+        return combi.slice(0, maxLength);
+    }
+    return combi;
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+
+
+
+
+
